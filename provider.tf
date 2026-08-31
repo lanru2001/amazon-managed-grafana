84556f88e52ad66f2c101dcb2defc3f1efc9a771
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2026-08-31 10:00:50 -0500

    Refactor provider configurations in provider.tf
    Updated provider configurations and versions for AWS, Grafana, Kubernetes, and Helm. Added default tags and authentication methods.

--- provider.tf
+++ provider.tf
@@ -1,36 +1,100 @@
-terraform {
-  required_version = ">= 1.0"
+provider "aws" {
+  region = "${local.region}"
 
+  default_tags {
+    tags = {
+      Terraform   = "true"
+      Project     = "Amazon Managed Grafana"
+    }
+  }
+}
+
+# provider "grafana" {
+#   alias = "amg"   #"cloud"
+#   url  = "https://${module.managed_grafana.workspace_endpoint}"
+#   auth = module.managed_grafana.workspace_service_account_tokens["terraform"].key #module.managed_grafana.workspace_service_account_tokens.terraform.key
+# }
+
+provider "grafana" {
+  alias = "cloud"
+  url  = "https://${module.managed_grafana.workspace_endpoint}"
+  auth = var.grafana_token
+  #auth = module.managed_grafana.workspace_service_account_tokens.terraform.key #module.managed_grafana.workspace_service_account_tokens["terraform"].key
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
     grafana = {
-      source  = "grafana/grafana"
-      version = "3.13.2"
+      source = "grafana/grafana"
+      version = "3.25.7"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+      version = ">=2.11.0"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+
+    time = {
+      source = "hashicorp/time"
+      version = "0.13.1"
+    }
+ 
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.4"
+    }
+
+  }
+}
+
+
+######################################################################################
+#K8S and Helm Provider
+######################################################################################
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.dlframe.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.dlframe.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = [ "eks", "get-token", "--cluster-name", "${var.cluster_name}", "--region", "${local.region}" ]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.dlframe.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.dlframe.certificate_authority[0].data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = [ "eks", "get-token", "--cluster-name", "${var.cluster_name}", "--region", "${local.region}" ]
+      command     = "aws"
     }
   }
-
-  backend "s3" {}
-  
 }
-provider "aws" {
-  region = "us-east-1"
 
-  default_tags {
-    tags = {
-      Environment = "staging"
-      Terraform   = "true"
-      Project     = "stag"
+provider "kubectl" {
+    host                   = data.aws_eks_cluster.dlframe.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.dlframe.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = [ "eks", "get-token", "--cluster-name", "${var.cluster_name}", "--region", "${local.region}" ]
+      command     = "aws"
     }
-  }
-}
-
-
-provider "grafana" {
-  alias = "cloud"
-
-  url  = "https://${module.managed_grafana.workspace_endpoint}"
-  auth = module.managed_grafana.workspace_service_account_tokens.terraform.key
 }
